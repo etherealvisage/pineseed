728e886f7935a56ebc52f626fe7ab44dba83637e
ethereal <ethereal@ethv.net>
2015-09-11 23:54:28 -0700

    Kept reworking event system, proxies partially implemented.

--- src/event/context.hpp
+++ src/event/context.hpp
@@ -17,33 +17,21 @@ class base_context {
 public:
     virtual ~base_context() {}
 
-    virtual void queue(const char *event_name, std::experimental::any params) = 0;
     virtual void fire_queued() = 0;
 };
 
 class default_proxy {
 public:
     template<typename ...T>
-    void proxy(std::string name, const std::experimental::any &params) {}
+    void proxy(std::string name, const std::tuple<T...> &params) {}
 };
 
 template<typename proxy_t = default_proxy>
 class proxy_context : public base_context {
 private:
-    std::multimap<std::string,
-        std::function<void (const std::experimental::any &)>> m_listeners;
-    std::vector<std::pair<std::string, std::experimental::any>> m_events;
+    std::multimap<std::string, std::experimental::any> m_listeners;
+    std::vector<std::function<void ()>> m_events;
     proxy_t m_proxy;
-private:
-    template<typename ...T>
-    static void invoker(const std::function<void (T...)> &function,
-        const std::experimental::any &params) {
-
-        std::tuple<T...> tparams =
-            std::experimental::any_cast<decltype(tparams)>(params);
-
-        std::experimental::apply(function, tparams);
-    }
 public:
     proxy_context() {}
     proxy_context(proxy_t &&proxy) : m_proxy(proxy) {}
@@ -52,12 +40,29 @@ public:
     void add_listener(const char *event_name,
         std::function<void (T...)> function) {
 
-        m_listeners.insert(std::make_pair(std::string(event_name),
-            std::bind(invoker<T...>, function, std::placeholders::_1)));
+        m_listeners.insert(std::make_pair(std::string(event_name), function));
     }
 
-    virtual void queue(const char *event_name, std::experimental::any params) {
-        m_events.push_back(std::make_pair(std::string(event_name), params));
+    template<typename ...T>
+    void invoke_all(std::string name, std::experimental::any params) {
+        std::tuple<T...> tparams
+            = std::experimental::any_cast<decltype(tparams)>(params);
+
+        auto it = m_listeners.find(name);
+        while(it != m_listeners.end() && it->first == name) {
+            auto f = std::experimental::any_cast<std::function<void (T...)>>(
+                it->second);
+            std::experimental::apply(f, tparams);
+            it ++;
+        }
+
+        m_proxy.proxy(name, tparams);
+    }
+
+    template<typename ...T>
+    void queue(const char *event_name, std::tuple<T...> params) {
+        m_events.push_back(std::bind(&proxy_context<proxy_t>::invoke_all<T...>,
+            this, std::string(event_name), params));
     }
 
     virtual void fire_queued() {
@@ -65,14 +70,12 @@ public:
         std::swap(events_copy, m_events);
 
         for(auto &event : events_copy) {
-            auto it = m_listeners.find(event.first);
-            while(it != m_listeners.end() && it->first == event.first) {
-                it->second(event.second);
-                it ++;
-            }
+            event();
         }
     }
 };
+
+using context = proxy_context<>;
 
 } // namespace event
 
